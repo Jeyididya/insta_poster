7fb6f6f8b0c7b7ed358fd3af9870b4691e7ab8ae
Yididya <jedidiahamdualem@gmail.com>
2022-11-28 11:34:37 +0300

    test 3

--- instagram_api.py
+++ instagram_api.py
@@ -1,8 +1,4 @@
 from instagrapi import Client
 cl = Client()
-cl.login("yididya051@gmail.com", "0113470047j")
+cl.login("quotewithpicturesdaily", "0113470047j")
 print("successfully logged in")
-
-
-
-
