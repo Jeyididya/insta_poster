2a295ebe74a80cf39639918daea4b5ac2938b519
Yididya <jedidiahamdualem@gmail.com>
2022-11-28 14:29:51 +0300

    test7

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from get_quotes import *
 from get_image import *
 from edit_photo import *
 
-from instagram_api import *
+# from instagram_api import *
 import textwrap
 from time import sleep
 
@@ -11,45 +11,55 @@ import random
 
 words=["friend","car","alone","lonely","sad","wealth"]
 
-def check_text(quote):
-    lines=textwrap.wrap(quote["quote"],56)
-    if len(lines)<4:
-        return True
-    return False
+# def check_text(quote):
+#     lines=textwrap.wrap(quote["quote"],56)
+#     if len(lines)<4:
+#         return True
+#     return False
 
-def start():
-    quote=QUOTE()
-    get_image=GET_IMAGE()
-    _quote=quote.get_quote()
-    if check_text(_quote):
-        file_name=get_image.search_image(random.choice(words))
-        print(file_name,_quote["quote"])
-        im=EDIT_IMAGE(file_name)
-        file_name_edited=im.add_text(_quote["quote"],_quote["author"])
-        return file_name_edited
-    else:
-        start()
+# def start():
+#     quote=QUOTE()
+#     get_image=GET_IMAGE()
+#     _quote=quote.get_quote()
+#     if check_text(_quote):
+#         file_name=get_image.search_image(random.choice(words))
+#         print(file_name,_quote["quote"])
+#         im=EDIT_IMAGE(file_name)
+#         file_name_edited=im.add_text(_quote["quote"],_quote["author"])
+#         return file_name_edited
+#     else:
+#         start()
 
-def post_insta(file_name):
-    photographer=file_name.split("b7")[1].split("_")[0]
-    caption="Photographer {},\nImage from Pexeles \nQuote from themotivate365".format(photographer)
-    cl.photo_upload(
-        file_name, 
-        caption
-        )
-    print("photo posted",file_name)
+# def post_insta(file_name):
+#     photographer=file_name.split("b7")[1].split("_")[0]
+#     caption="Photographer {},\nImage from Pexeles \nQuote from themotivate365".format(photographer)
+#     cl.photo_upload(
+#         file_name, 
+#         caption
+#         )
+#     print("photo posted",file_name)
 
-times=[10,13,11,6,]
+# times=[10,13,11,6]
 
-while True:
-    file_=open("image_id.txt","r").readlines()
-    for id in file_:
-        print("id",id)
-    filename=start()
+# while True:
+#     # file_=open("image_id.txt","r").readlines()
+#     # for id in file_:
+#     #     print("id",id)
+#     filename=start()
 
-    if filename!=None:
-        post_insta(filename)
-    sleep_time=random.choice(times)
-    print("sleeping for ",sleep_time)
-    sleep(int(sleep_time)*3600)
+#     if filename!=None:
+#         post_insta(filename)
+#     sleep_time=random.choice(times)
+#     print("sleeping for ",sleep_time)
+#     sleep(int(sleep_time)*3600)
 
+get_image=GET_IMAGE()
+print("-->GOT",get_image.search_image("friends"))
+
+import os
+for a,b,c in os.walk("."):
+    print("###############")
+    print("a",a)
+    print("b",b)
+    print("c",c)
+    print("---------------")
